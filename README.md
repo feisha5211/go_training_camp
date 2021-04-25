0e099f3c8ae9d97e0b1f2b6fd8c2b66fcd9be2dd
wangwt1 <wangwt1@ziroom.com>
2021-04-25 22:59:32 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go_training_camp
-go_training_camp
+极客时间go语言训练营1期
---
+++ week2/sql_error.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+//dao遇到 sql.ErrNoRows 不应改wrap，应该直接处理，不返回数据即可
+
+func main() {
+	db, err := sql.Open("mysql", "dev_web:z@tcp(10.30.7.108:3306)/wxziroom");
+	defer db.Close()
+
+	if err != nil {
+		panic(err)
+	}
+	id := 1
+	nickname, err := queryDataWithId(db, id)
+	fmt.Println(nickname, err)
+}
+
+func queryDataWithId(db *sql.DB, id int) (string, error) {
+	var nickname string
+	err := db.QueryRow("SELECT nickname FROM hd_join WHERE id=?", id).Scan(&nickname)
+	switch {
+	case err == sql.ErrNoRows:
+		return "", nil
+	case err != nil:
+		return "", err
+	default:
+		return nickname, nil
+	}
+}
+
